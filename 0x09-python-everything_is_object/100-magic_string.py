60a275aa8a793504adbad8cd2e1d3a35b9c916f9
Favour-henry <nnakeefavour2022@gmail'com>
2023-04-05 21:02:30 -0700

    more classes and objects

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(my_list=[]):
+    my_list.append("Holberton")
+    return ", ".join(my_list)
---
+++ 0x09-python-everything_is_object/19-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+copy_list = __import__('19-copy_list').copy_list
+
+my_list = [1, 2, 3]
+print(my_list)
